8226c8df3f68c0200ada766d8810d8e9a1f6067d
lexsonn <Daimen.n.estevez@gmail.com>
2016-10-22 17:53:46 -0500

    Added put url

--- src/ng/domain/initialRegistration/NeedRegistrationService.js
+++ src/ng/domain/initialRegistration/NeedRegistrationService.js
@@ -3,9 +3,12 @@ angular.module('app').service('NeedRegistrationService', ['$http', function($htt
     this.person = {};
     this.previous = "/home/";
 
+    var url = "http://globalhack.0x3f8.com:32039/personInNeed/"
     var svc = this;
 
     this.addUser = function(person) {
+        person.phoneNumbers = [];
+        person.phoneNumbers.push(person.phone);
         return $http.put(url, person).then(function(result) {
             svc.person = result;
             return result;
