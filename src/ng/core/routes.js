f376d9f04d01dfd2ca094b53a8e2d2ecae3fd45b
Chad Michael Middleton <CMMIDD@monsanto.com>
2016-10-22 12:57:41 -0500

    Added fields for company form

--- src/ng/core/routes.js
+++ src/ng/core/routes.js
@@ -7,6 +7,10 @@ angular.module('app').config(['$routeProvider', 'baseRoute', function config($ro
             templateUrl: baseRoute + "initialRegistration/initRegistrationTemplate.html",
             controller: 'InitialRegistrationController',
             controllerAs: 'initialRegistrationController'
+        }).when('/service/register', {
+            templateUrl: baseRoute + "company-register/company-template.html",
+            controller: 'company-register-controller',
+            controllerAs: 'companyRegisterController'
         }).otherwise('/home');
     }
 ]);
