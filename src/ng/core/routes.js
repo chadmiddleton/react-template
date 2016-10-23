97f410b6cf2bcddc36c9c4c89342c313517989e8
Chad Michael Middleton <CMMIDD@monsanto.com>
2016-10-22 19:08:56 -0500

    Added folders for client and provider list with files. Also added both to the router

--- src/ng/core/routes.js
+++ src/ng/core/routes.js
@@ -1,24 +1,32 @@
 angular.module('app').config(['$routeProvider', 'baseRoute', function config($routeProvider, baseRoute) {
-        $routeProvider.when('/home', {
-            templateUrl: baseRoute + "home/homeTemplate.html",
-            controller: 'HomeController',
-            controllerAs: 'homeController'
-        }).when('/help/register', {
-            templateUrl: baseRoute + "initialRegistration/initRegistrationTemplate.html",
-            controller: 'InitialRegistrationController',
-            controllerAs: 'initialRegistrationController'
-        }).when('/help/register-name', {
-            templateUrl: baseRoute + "initialRegistration/registerName.html",
-            controller: 'InitialRegistrationController',
-            controllerAs: 'initialRegistrationController'
-        }).when('/help/register-personal', {
-            templateUrl: baseRoute + "initialRegistration/registerPersonal.html",
-            controller: 'InitialRegistrationController',
-            controllerAs: 'initialRegistrationController'
-        }).when('/service/register', {
-            templateUrl: baseRoute + "company-register/company-template.html",
-            controller: 'company-register-controller',
-            controllerAs: 'companyRegisterController'
-        }).otherwise('/home');
-    }
+    $routeProvider.when('/home', {
+        templateUrl: baseRoute + "home/homeTemplate.html",
+        controller: 'HomeController',
+        controllerAs: 'homeController'
+    }).when('/help/register', {
+        templateUrl: baseRoute + "initialRegistration/initRegistrationTemplate.html",
+        controller: 'InitialRegistrationController',
+        controllerAs: 'initialRegistrationController'
+    }).when('/help/register-name', {
+        templateUrl: baseRoute + "initialRegistration/registerName.html",
+        controller: 'InitialRegistrationController',
+        controllerAs: 'initialRegistrationController'
+    }).when('/help/register-personal', {
+        templateUrl: baseRoute + "initialRegistration/registerPersonal.html",
+        controller: 'InitialRegistrationController',
+        controllerAs: 'initialRegistrationController'
+    }).when('/provider/register', {
+        templateUrl: baseRoute + "company-register/company-template.html",
+        controller: 'company-register-controller',
+        controllerAs: 'companyRegisterController'
+    }).when('/provider/list', {
+        templateUrl: baseRoute + "providerList/providerListTemplate.html",
+        controller: 'providerListController',
+        controllerAs: 'providerListController'
+    }).when('/help/list', {
+        templateUrl: baseRoute + "clientList/clientListTemplate.html",
+        controller: 'clientListController',
+        controllerAs: 'clientListController'
+    }).otherwise('/home');
+}
 ]);
--- src/ng/domain/company-register/company-register-controller.js
+++ src/ng/domain/company-register/company-register-controller.js
@@ -1,3 +1,68 @@
 angular.module('app').controller('company-register-controller',
-    ['$route', '$scope', function ($route, $scope) {
+    ['$route', '$scope', '$http', function ($route, $scope, $http) {
+        
+        $scope.provider = {};
+        // setInterval(function () {
+        //     console.log($scope.provider)
+        // }, 5000);
+        $scope.provider.agencyName = "";
+        $scope.provider.agencyStreet = "";
+        $scope.provider.agencyCity = "";
+        $scope.provider.agenctyState = "";
+        $scope.provider.agencyZip = "";
+        $scope.provider.agencyPhone = "";
+        $scope.provider.agencyWebsite = "";
+
+        $scope.provider.nonProfitStatus = "";
+        $scope.provider.governmentEntity = "";
+        $scope.provider.faithBased = "";
+
+        $scope.provider.agencyExecName = "";
+        $scope.provider.agencyExecNumber = "";
+        $scope.provider.agencyExecEmail = "";
+
+        $scope.provider.eligibilityCriteria = "";
+        $scope.provider.admissionContactName = "";
+        $scope.provider.admissionContactNumber = "";
+        $scope.provider.admissionContactEmail = "";
+
+        $scope.provider.hoursOfOperation = "";
+        $scope.provider.bedNumber = "";
+        $scope.provider.unitNumber = "";
+        $scope.provider.peopleServed = "";
+
+        $scope.provider.popServed = {
+            "Male": false,
+            "Female": false,
+            "Adolescents": false,
+            "Families": false,
+            "Women_Children": false,
+            "Veterans": false,
+            "Dually_Diagnosed": false,
+            "Domestic_Violence": false,
+            "Pregnant_Women": false,
+            "HIV_AIDS": false,
+            "Homeless": false,
+            "Substance_Abuse": false,
+            "Mental_Illness": false,
+            "Developmentally_Delayed": false,
+            "Disabled": false,
+            "Other": false
+        };
+
+        this.returnHome = function () {
+            $location.path('/home')
+        };
+
+        const url = 'http://globalhack.0x3f8.com:32039/provider';
+
+        this.register = function (provider) {
+            return $http.put(url, provider).then(function (result) {
+                console.log(provider);
+                console.log(result);
+                return result;
+            })
+        }
+
+
     }]);
